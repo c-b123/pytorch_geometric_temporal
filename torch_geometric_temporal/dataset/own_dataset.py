590ce5b42e08e70cded406926b6c2115effc9200
Christian <christian.berger2@uzh.ch>
2024-04-30 14:59:36 +0200

    Train val test splitter for StaticDatasetLoader implemented
    own_dataset.py:
    - train val test splitter implemented
    - improvements on standardization implementation
    - dimensionality check for unstandardize function added
    - some refactoring

--- torch_geometric_temporal/dataset/own_dataset.py
+++ torch_geometric_temporal/dataset/own_dataset.py
@@ -3,6 +3,8 @@ import requests
 import numpy as np
 import torch
 
+from torch_geometric_temporal.signal import temporal_signal_val_split
+from torch_geometric_temporal.signal import temporal_signal_split
 from torch_geometric_temporal.signal import StaticGraphTemporalSignal
 
 
@@ -11,6 +13,9 @@ class StaticDatasetLoader(object):
     def __init__(self, path, colab=False):
         self.lags = None
         self.offset = None
+        self.val_ratio = 0
+        self.test_ratio = 0
+        self._signal = None
         self._read_web_data(path, colab)
 
     def _read_web_data(self, path, colab):
@@ -36,8 +41,9 @@ class StaticDatasetLoader(object):
 
     def _standardize_dataset(self):
         data = np.array(self._dataset["FX"])
-        self.mean = np.mean(data, axis=0)
-        self.std = np.std(data, axis=0)
+        train_snapshots = int((1 - self.val_ratio - self.test_ratio) * data.shape[0])
+        self.mean = np.mean(data[0:train_snapshots], axis=0)
+        self.std = np.std(data[0:train_snapshots], axis=0)
 
         self._dataset["FX"] = (data - self.mean) / self.std
 
@@ -78,7 +84,7 @@ class StaticDatasetLoader(object):
             for i in range(num_snapshots)
         ]
 
-    def get_dataset(self, lags: int = 4, offset: int = 1, standardize: bool = True) -> StaticGraphTemporalSignal:
+    def get_dataset(self, lags: int = 4, offset: int = 1, standardize: bool = False) -> StaticGraphTemporalSignal:
         """Returning the Chickenpox Hungary data iterator.
 
         Args types:
@@ -87,6 +93,10 @@ class StaticDatasetLoader(object):
         Return types:
             * **dataset** *(StaticGraphTemporalSignal)* - The Chickenpox Hungary dataset.
         """
+        self.prepare_dataset(lags=lags, offset=offset, standardize=standardize)
+        return self._signal
+
+    def prepare_dataset(self, lags: int = 4, offset: int = 1, standardize: bool = False):
         self.lags = lags
         self.offset = offset
         if standardize:
@@ -97,17 +107,27 @@ class StaticDatasetLoader(object):
             self._get_targets_and_features_with_offset()
         else:
             self._get_targets_and_features()
-        dataset = StaticGraphTemporalSignal(
-            self._edges, self._edge_weights, self.features, self.targets
-        )
-        return dataset
+        self._signal = StaticGraphTemporalSignal(self._edges, self._edge_weights, self.features, self.targets)
+
+    def get_train_test_split(self, lags: int = 4, offset: int = 1, standardize: bool = False, test_ratio: float = 0.2):
+        self.prepare_dataset(lags=lags, offset=offset, standardize=standardize)
+        return temporal_signal_split(self._signal, train_ratio=1-test_ratio)
+
+    def get_train_val_test_split(self, lags: int = 4, offset: int = 1, standardize: bool = False,
+                                 val_ratio: float = 0.1, test_ratio: float = 0.1):
+        self.prepare_dataset(lags=lags, offset=offset, standardize=standardize)
+        return temporal_signal_val_split(self._signal, val_ratio=val_ratio, test_ratio=test_ratio)
 
     def unstandardize(self, pred: torch.Tensor):
-        # TODO: dimensionality check
-        print(self.std.shape)
-        return np.multiply(pred, self.std) + self.mean
+        assert pred.shape[0] == self.features[0].shape[0]
+        result = np.multiply(pred.squeeze(), self.std) + self.mean
+        return result
 
 
 if __name__ == "__main__":
     loader = StaticDatasetLoader("Resources/test_data.json")
-    dataset = loader.get_dataset(lags=2, offset=3, standardize=False)
+    # train, test = loader.get_train_test_split(lags=2, offset=3, standardize=False, test_ratio=0.2)
+    data = loader.get_dataset(lags=2, offset=3, standardize=True)
+    test_tensor = torch.tensor([[1], [2], [3]])
+    un = loader.unstandardize(test_tensor)
+    a = 54
